145348fe0d0bc7a721f4b6f51f6492637c1b1b7a
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2015-05-21 11:28:04 +0200

    Update uebng1.asm

--- uebng1.asm
+++ uebng1.asm
@@ -1,47 +1,47 @@
-		AREA Test, CODE
-		INCLUDE LPC2368_asm.inc
-		ENTRY
-		EXPORT start	
-
+	AREA	 arm1, CODE
+	INCLUDE 	LPC2368_asm.inc
+	ENTRY 
+	EXPORT start	
+	
+	
 start
-
-	MOV R4, #0x00000FF ;alles als Output bis Bit 10 auf Input
-	MOV R8, #0x00000400 ; 
-
-	LDR R2,=FIO2_DIR ;jetzt funktioniert R2 als ein pointer 
-	LDR R3,=FIO2_PIN ;Pointer auf Input Pins
 	
-	;EInstellungen
-	LDR R5,[R2]
-	ORR R5,R5,R4 ; R5 = R5 | R4 -> R5 ist der Inhalt aus FIO2_DIR 
-	STR R5,[R2]
+	LDR R0, =FIO2_DIR	;pointer uebergeben an R0, also zeigt R0 auf FIO_DIR und dessen Inhalt
+	LDR R2, [R0]		;Tut sich hier schon nichts?? Inhalt von FIO2_DIR in Rs speichern zum lesen
+	MOV R3, #17			;P2.0 und P2.02 sollen ein Ausgang '1' und P2.10 ein Eingang '0' --> ...00000101 = 5
 	
+	ORR R3, R2, R3		;setzen der bits 0,2,10 zur einstellung und erhaltung der anderen Daten
+	STR R3, [R0]		;Wird nicht verarbeitet?? zurückschreiben der Einstellung in das FIO2_DIR über Adresse 
+	
+	LDR R1, =FIO2_PIN
+	
+loop
+	
+	LDR R4, [R1]		;lädt einen falschen Wert rein
 
-	MOV R6, #0x00000005
-	MOV R5, #0xFFFFFFFA ;R5 ist R4 invertiert
-
-
-loop_start
-		 
-	LDR R1,[R3] ;Eingangsports holen
-	AND R7,R1,R8 ;R7 = R1 & R4
-
-	CMP R7,#1024                 
+	AND R5, R4, #0x00000400
+	CMP R5, #1024
+	
+;	ANDEQ R1, R4, #0xFFFFFEE
+	
+;	ORRNE R1, R4, #0x00000101
+	
+	;TST R4, #10			;Er überspringt diesen Teil NE --> not equal = '0' EQ--> equal
+	
+	BNE gedrueckt
+	BEQ nicht_gedrueckt
+	;AND R3, R4, #0xFFFFFFFA
+	;STR R3, [R1]
 	
 	
-	BNE LED_AN
-    BEQ LED_AUS
-	BL loop_start
-
-
-
-LED_AN
-	ORR R1,R1,R6
-	STR R1,[R3]
-	BL loop_start
-LED_AUS
-	AND R1,R1,R5
-	STR R1,[R3]
-	BL loop_start
+	BL loop
 	
+gedrueckt	
+	ORR R3, R4, #17
+	STR R3, [R1]
+	BL loop
+nicht_gedrueckt
+	AND R3,R4,#0xFFFFFFEE
+	STR R3,[R1]
+	BL loop
 	END
